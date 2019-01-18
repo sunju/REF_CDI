43eea63021183fff205e1f5368020241c63a067f
david-bar <davidbar@stanford.edu>
2019-01-17 16:53:15 -0800

    Add files via upload

---
+++ data_freq_dist_flatX_1.m
@@ -0,0 +1,37 @@
+clear all
+rng(1)
+n=64;
+m=1024;
+X = imread('mimivirus.png');
+X = mat2gray(X);
+X_0 = rgb2gray(X);
+img=imresize(X_0,[n,n]);
+%%%
+delta_rad=2;
+img=zeros(n);
+img(n/2-delta_rad:n/2+delta_rad,n/2-delta_rad:n/2+delta_rad)=ones(2*delta_rad+1,2*delta_rad+1);
+%%%%
+n=size(img,1); % assuming image is square
+bref=ones(n,n);
+pref=zeros(n,n); pref(end,end)=1;
+sref=zeros(n,n); sref(:,end)=ones(n,1);
+xb=[img,bref];
+xbpad=zeros(m,m); xbpad(1:n,1:2*n)=xb;
+yb=abs(fft2(xbpad)).^2;
+xp=[img,pref];
+xppad=zeros(m,m); xppad(1:n,1:2*n)=xp;
+yp=abs(fft2(xppad)).^2;
+xs=[img,sref];
+xspad=zeros(m,m); xspad(1:n,1:2*n)=xs;
+ys=abs(fft2(xspad)).^2;
+%%
+figure;
+subplot(3,1,1)
+stem3([-512:511],[-512:511],fftshift(yb),'red')
+title('Squared Four. magnitudes for delta with block ref.')
+subplot(3,1,2)
+stem3([-512:511],[-512:511],fftshift(ys),'blue')
+title('Squared Four. magnitudes for delta with slit ref.')
+subplot(3,1,3)
+stem3([-512:511],[-512:511],fftshift(yp),'magenta')
+title('Squared Four. magnitudes for delta with pinhole ref.')
---
+++ delta_spec.m
@@ -0,0 +1,19 @@
+clear all
+rng(1)
+n=64;
+m=1024;
+X = imread('mimivirus.png');
+X = mat2gray(X);
+X_0 = rgb2gray(X);
+img=imresize(X_0,[n,n]);
+%%%
+delta_rad=2;
+img=zeros(n);
+img(n/2-delta_rad:n/2+delta_rad,n/2-delta_rad:n/2+delta_rad)=ones(2*delta_rad+1,2*delta_rad+1);
+%%%%
+imgpad=zeros(m,m); imgpad(1:n,1:n)=img;
+y=abs(fft2(imgpad)).^2;
+%%
+figure;
+stem3([-512:511],[-512:511],fftshift(y))
+title('Squared Four. magnitudes for delta image')
