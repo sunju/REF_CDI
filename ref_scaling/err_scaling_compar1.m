9f331cc84988b730bbd4e9e5f1662fcbd1dcd651
david-bar <davidbar@stanford.edu>
2019-01-18 23:06:54 -0800

    Add files via upload

---
+++ ref_scaling/err_scaling_compar1.m
@@ -0,0 +1,32 @@
+% Reproduces Figure 7a of the paper
+% "Holographic Phase Retrieval and Optimal Reference Design",
+% by David Barmherzig, Ju Sun, Emmanuel Candes, T.J. Lane, and Po-Nan Li.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+n=64;
+m=1024;
+bndry=500;
+for k1=-bndry:bndry
+    for k2=-bndry:bndry
+        lower_bnd(k1+bndry+1,k2+bndry+1)=1/(m^4);
+        scaler_b(k1+bndry+1,k2+bndry+1)=( (1/(m^2))+2*((n-1)/(m^2))*(1-cos(2*pi*k1/m)) )*( (1/(m^2))+2*((n-1)/(m^2))*(1-cos(2*pi*k2/m)));
+        scaler_p(k1+bndry+1,k2+bndry+1)=((n^2)/(m^4));
+        scaler_s(k1+bndry+1,k2+bndry+1)=(n/(m^2))*( (1/(m^2))+2*((n-1)/(m^2))*(1-cos(2*pi*k2/m)));
+    end
+end
+figure;
+stem3([-bndry:bndry],[-bndry:bndry],lower_bnd,'red')
+hold on;
+stem3([-bndry:bndry],[-bndry:bndry],scaler_b,'blue')
+hold on;
+stem3([-bndry:bndry],[-bndry:bndry],scaler_p,'green')
+hold on;
+stem3([-bndry:bndry],[-bndry:bndry],scaler_s,'magenta')
+hold off
+%direction = [1 1 0];
+%rotate(hSurface,direction,25)
+legend({'Uniform lower bound','Block ref.', 'Pinhole ref.', 'Slit ref.'},'FontSize',14)
+xlabel('x-frequency','FontSize',18)
+ylabel('y-frequency','FontSize',18)
+%ylim([0,1e-5])
---
+++ ref_scaling/err_scaling_compar_cross_sec_y_updated.m
@@ -0,0 +1,26 @@
+% Reproduces Figure 7c of the paper
+% "Holographic Phase Retrieval and Optimal Reference Design",
+% by David Barmherzig, Ju Sun, Emmanuel Candes, T.J. Lane, and Po-Nan Li.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%clear all
+n=64;
+m=1024;
+bndry=511;
+for k1=-bndry:bndry+1
+    k2=0;
+    lower_bnd(k1+bndry+1)=1/(m^4);
+    scaler_b(k1+bndry+1)=( (1/(m^2))+2*((n-1)/(m^2))*(1-cos(2*pi*k1/m)) )*( (1/(m^2))+2*((n-1)/(m^2))*(1-cos(2*pi*k2/m)));
+    scaler_p(k1+bndry+1)=((n^2)/(m^4));
+    scaler_s(k1+bndry+1)=(n/(m^2))*( (1/(m^2))+2*((n-1)/(m^2))*(1-cos(2*pi*k2/m)));
+end
+figure;
+plot([-bndry:bndry+1],lower_bnd,'red','LineWidth',2)
+hold on;
+plot([-bndry:bndry+1],scaler_b,'blue','LineWidth',2)
+hold on;
+plot([-bndry:bndry+1],scaler_p,'green','LineWidth',2)
+hold on;
+plot([-bndry:bndry+1],scaler_s,'magenta','LineWidth',2)
+hold off
+legend({'Uniform lower bound','Block ref.', 'Pinhole ref.', 'Slit ref.'},'FontSize',14)
+xlabel('y-frequency','FontSize',18)
+xlim([-512,512])
